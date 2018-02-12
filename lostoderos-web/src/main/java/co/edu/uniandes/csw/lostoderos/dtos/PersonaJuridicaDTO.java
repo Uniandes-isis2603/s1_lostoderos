7308068c246b0e0eb6a305da46d8ffbf0dd8b0b9
na.morenoe <na.morenoe@ISIS2603S1-0079.sis.virtual.uniandes.edu.co>
2018-02-12 09:01:22 -0500

    Documentacion

--- lostoderos-web/src/main/java/co/edu/uniandes/csw/lostoderos/dtos/PersonaJuridicaDTO.java
+++ lostoderos-web/src/main/java/co/edu/uniandes/csw/lostoderos/dtos/PersonaJuridicaDTO.java
@@ -6,7 +6,28 @@
 package co.edu.uniandes.csw.lostoderos.dtos;
 
 /**
+ * PersonaJuridicalDTO Objeto de transferencia de datos de la entidad de PersonaJuridica. Los DTO contienen las
+ * represnetaciones de los JSON que se transfieren entre el cliente y el servidor.
+ * 
+ * <p>
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "id": Number,
+ *      "NIT": String,
+ *   }
+ * </pre>
+ * Por ejemplo una entidad de Servicio se representa asi:<br>
+ * <p>
+ * <pre>
  *
+ *   {
+ *      "id": 15453,
+ *      "NIT": "1022434817",
+ *   }
+ *
+ * </pre>
+ * 
  * @author na.morenoe
  */
 public class PersonaJuridicaDTO {
@@ -22,10 +43,16 @@ public class PersonaJuridicaDTO {
         this.ID = ID;
     }
 
+    /**
+    * @return El NIT de la entidad PersonaJuridica
+     */
     public String getNIT() {
         return NIT;
     }
 
+        /**
+    * @param NIT El nuevo NIT
+    */
     public void setNIT(String NIT) {
         this.NIT = NIT;
     }
--- lostoderos-web/src/main/java/co/edu/uniandes/csw/lostoderos/dtos/PersonaNaturalDTO.java
+++ lostoderos-web/src/main/java/co/edu/uniandes/csw/lostoderos/dtos/PersonaNaturalDTO.java
@@ -6,9 +6,31 @@
 package co.edu.uniandes.csw.lostoderos.dtos;
 
 /**
+ * PersonaNaturalDTO Objeto de transferencia de datos de la entidad de PersonaNatural. Los DTO contienen las
+ * represnetaciones de los JSON que se transfieren entre el cliente y el servidor.
+ * 
+ * <p>
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ *   {
+ *      "id": Number,
+ *      "Cedula": String,
+ *   }
+ * </pre>
+ * Por ejemplo una entidad de Servicio se representa asi:<br>
+ * <p>
+ * <pre>
  *
+ *   {
+ *      "id": 15453,
+ *      "Cedula": "1022434817",
+ *   }
+ *
+ * </pre>
+ * 
  * @author na.morenoe
  */
+
 public class PersonaNaturalDTO {
     
     private long ID;
