5995fce6b929cb17ba9580e17dd1efd8ace48a58
SANTIAGO BLANCO CEDIEL <s.blancoc@sis.virtual.uniandes.edu.co>
2018-02-12 10:37:55 -0500

    Clases DTO

---
+++ lostoderos-web/src/main/java/co/edu/uniandes/csw/lostoderos/dtos/CalificacionDTO.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.lostoderos.dtos;
+/**
+ * CalificacionDTO Objeto de transferencia de datos de la entidad de
+ * calificacion. Los DTO contienen las representaciones de los JSON que se
+ * transfieren entre el cliente y el servidor. 
+ * <p>
+ * Al serializarse como JSON esta clase implementa el siguiente modelo: <br>
+ * <pre>
+ * {
+ *  "numEstrellas": number
+ *  "comentario": String
+ *  "tipoServicio": String
+ * }
+ * </pre>
+ Por ejemplo una entidad de Calificacion se representa asi:<br>
+ * <p>
+ * <pre>
+ *
+ *   {
+ *      "numEstrellas": 3,
+ *      "comentario": "Llego despues de la hora programada",
+ *      "tipoServicio": "plomeria"
+ *   }
+ *
+ * </pre>
+ *
+ * @author s.blancoc
+ */
+public class CalificacionDTO {
+    
+    /**
+     * Numero de estrellas dadas por la calificación
+     */
+    private Integer numEstrellas;
+    
+    /**
+     * tipo de servicio que está siendo calificado
+     */
+    private String tipoServicio;
+    
+    /**
+     * comentario asociado a la calificacion del servicio
+     */
+    private String comentario;
+    
+    /**
+     * Constructor vacio
+     */
+    public CalificacionDTO(){
+        
+    }
+    /**
+     * 
+     * @return el comentario de la calificacion
+     */
+    public String getComentario(){
+        return comentario;
+    }
+    /**
+     * 
+     * @return el tipo de servicio que está siendo calificado 
+     */
+    public String getTipoServicio(){
+        return tipoServicio;
+    }
+    /**
+     * 
+     * @return el numero de estrellas dadas en la calificacion 
+     */
+    public Integer getNumEstrellas(){
+        return numEstrellas;
+    }
+    /**
+     * 
+     * @param numEstrellas numero de estrellas para la calificacion 
+     */
+    public void setNumEstrellas(Integer numEstrellas){
+        this.numEstrellas = numEstrellas;
+    }
+    /**
+     * 
+     * @param comentario el comentario para la calificacion 
+     */
+    public void setComentario(String comentario){
+        this.comentario = comentario;
+    }
+    /**
+     * 
+     * @param tipoServicio el tipo de servicio  
+     */
+    public void setTipoServicio(String tipoServicio){
+        this.tipoServicio = tipoServicio;
+    }
+    
+}
