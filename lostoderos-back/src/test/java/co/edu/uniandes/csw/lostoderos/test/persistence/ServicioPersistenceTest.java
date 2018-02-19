f9e01a4235beb6e1b7e6f14b19a21d393e68c57c
m.saaravia <m.saravia@uniandes.edu.co>
2018-02-19 08:50:21 -0500

    entities

---
+++ lostoderos-back/src/test/java/co/edu/uniandes/csw/lostoderos/test/persistence/ServicioPersistenceTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.lostoderos.test.persistence;
+
+/**
+ *
+ * @author s.naranjop1
+ */
+public class ServicioPersistenceTest {
+    
+}
