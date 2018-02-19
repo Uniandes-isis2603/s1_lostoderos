fd0271b213a6a72b39411430249015a14ac07aff
Sergio Naranjo Puentes <s.naranjop1@uniandes.edu.co>
2018-02-19 13:16:28 -0500

    ..

---
+++ lostoderos-back/src/test/java/co/edu/uniandes/csw/lostoderos/test/persistence/ServicioPersistenceTest.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.lostoderos.test.persistence;
+
+import co.edu.uniandes.csw.lostoderos.entities.ServicioEntity;
+import co.edu.uniandes.csw.lostoderos.persistence.ServicioPersistence;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author s.naranjop1
+ */
+@RunWith(Arquillian.class)
+public class ServicioPersistenceTest 
+{
+    /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de XYZ, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ServicioEntity.class.getPackage())
+                .addPackage(ServicioPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    public ServicioPersistenceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of create method, of class ServicioPersistence.
+     */
+    @Test
+    public void testCreate() {
+    }
+
+    /**
+     * Test of find method, of class ServicioPersistence.
+     */
+    @Test
+    public void testFind() {
+    }
+
+    /**
+     * Test of update method, of class ServicioPersistence.
+     */
+    @Test
+    public void testUpdate() {
+    }
+
+    /**
+     * Test of delete method, of class ServicioPersistence.
+     */
+    @Test
+    public void testDelete() {
+    }
+    
+}
