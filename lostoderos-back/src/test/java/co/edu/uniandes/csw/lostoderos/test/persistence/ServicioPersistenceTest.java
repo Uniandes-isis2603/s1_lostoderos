f84b0526a189f3c19f57f537f8e0d7a83cab7790
Sergio Naranjo Puentes <s.naranjop1@uniandes.edu.co>
2018-02-19 09:15:09 -0500

    FIX

--- lostoderos-back/src/test/java/co/edu/uniandes/csw/lostoderos/test/persistence/ServicioPersistenceTest.java
+++ lostoderos-back/src/test/java/co/edu/uniandes/csw/lostoderos/test/persistence/ServicioPersistenceTest.java
@@ -5,10 +5,52 @@
  */
 package co.edu.uniandes.csw.lostoderos.test.persistence;
 
+import co.edu.uniandes.csw.lostoderos.entities.ServicioEntity;
+import co.edu.uniandes.csw.lostoderos.persistence.ServicioPersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import junit.framework.Assert;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author s.naranjop1
  */
-public class ServicioPersistenceTest {
+@RunWith(Arquillian.class)
+public class ServicioPersistenceTest 
+{
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(AuthorEntity.class.getPackage())
+                .addPackage(AuthorPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+
+ 
+    @Inject
+    private ServicioPersistence servicioPersistence;
+
+ 
+    @PersistenceContext
+    private EntityManager em;
     
+    @Test
+    public void createAuthorTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        ServicioEntity newEntity = factory.manufacturePojo(ServicioEntity.class);
+        ServicioEntity result = ServicioPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        ServicioEntity entity = em.find(ServicioEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
 }
