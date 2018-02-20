98b76fa13321289045c8a4f1bffdb7edf5d8deba
SERGIO ANDRES YEPES JOVEN <sa.yepes@sis.virtual.uniandes.edu.co>
2018-02-19 22:58:02 -0500

    Test de persistencia de hoja de vida y contratista implementada

---
+++ lostoderos-back/src/test/java/co/edu/uniandes/csw/lostoderos/test/persistence/ContratistaPersistenceTest.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.lostoderos.test.persistence;
+
+import co.edu.uniandes.csw.lostoderos.entities.ContratistaEntity;
+import co.edu.uniandes.csw.lostoderos.persistence.ContratistaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author sa.yepes
+ */
+@RunWith(Arquillian.class)
+public class ContratistaPersistenceTest {
+    /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Editorial, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(ContratistaEntity.class.getPackage())
+                .addPackage(ContratistaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Inyección de la dependencia a la clase ContratistaPersistence cuyos métodos
+     * se van a probar.
+     */
+    @Inject
+    private ContratistaPersistence contratistaPersistence;
+
+    /**
+     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
+     * datos por fuera de los métodos que se están probando.
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+    
+    /**
+     * Configuración inicial de la prueba.
+     *
+     *
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from ContratistaEntity").executeUpdate();
+    }
+
+    /**
+     * lista que tiene los datos de prueba
+     */
+    private List<ContratistaEntity> data = new ArrayList<ContratistaEntity>();
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            
+            ContratistaEntity entity = factory.manufacturePojo(ContratistaEntity.class);
+
+            em.persist(entity);
+            
+            data.add(entity);
+        }
+    }
+
+    /**
+     * Prueba para crear un Contratista.
+     *
+     *
+     */
+    @Test
+    public void createEditorialTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        ContratistaEntity newEntity = factory.manufacturePojo(ContratistaEntity.class);
+        ContratistaEntity result = contratistaPersistence.create(newEntity);
+        Assert.assertNotNull(result);
+        ContratistaEntity entity = em.find(ContratistaEntity.class, result.getId());
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+}
---
+++ lostoderos-back/src/test/java/co/edu/uniandes/csw/lostoderos/test/persistence/HojaDeVidaPersistenceTest.java
@@ -0,0 +1,160 @@
+/*
+MIT License
+
+Copyright (c) 2017 Universidad de los Andes - ISIS2603
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+package co.edu.uniandes.csw.lostoderos.test.persistence;
+
+import co.edu.uniandes.csw.lostoderos.entities.HojaDeVidaEntity;
+import co.edu.uniandes.csw.lostoderos.persistence.HojaDeVidaPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author sa.yepes
+ */
+@RunWith(Arquillian.class)
+public class HojaDeVidaPersistenceTest {
+     /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de HojaDeVida, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(HojaDeVidaEntity.class.getPackage())
+                .addPackage(HojaDeVidaPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Inyección de la dependencia a la clase HojaDeVidaPersistence cuyos métodos
+     * se van a probar.
+     */
+    @Inject
+    private HojaDeVidaPersistence hojaDeVidaPersistence;
+    
+     /**
+     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
+     * datos por fuera de los métodos que se están probando.
+     */
+    @PersistenceContext
+    private EntityManager em;
+    
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+    
+     /**
+     * Configuración inicial de la prueba.
+     *
+     *
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from HojaDeVidaEntity").executeUpdate();
+    }
+    
+    /**
+     * lista que tiene los datos de prueba
+     */
+    private List<HojaDeVidaEntity> data = new ArrayList<HojaDeVidaEntity>();
+    
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            
+            HojaDeVidaEntity entity = factory.manufacturePojo(HojaDeVidaEntity.class);
+
+            em.persist(entity);
+            
+            data.add(entity);
+        }
+    }
+    
+    /**
+     * Prueba para crear una HojaDeVida.
+     *
+     *
+     */
+    @Test
+    public void createEditorialTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        HojaDeVidaEntity newEntity = factory.manufacturePojo(HojaDeVidaEntity.class);
+        HojaDeVidaEntity result = hojaDeVidaPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        HojaDeVidaEntity entity = em.find(HojaDeVidaEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+}
