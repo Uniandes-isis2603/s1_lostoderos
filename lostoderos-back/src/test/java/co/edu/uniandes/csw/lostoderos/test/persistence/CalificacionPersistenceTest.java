89c4cd6d72b250994305d4eff94f993a2931af99
SANTIAGO BLANCO CEDIEL <s.blancoc@sis.virtual.uniandes.edu.co>
2018-02-19 22:43:14 -0500

    Tests persistencia

---
+++ lostoderos-back/src/test/java/co/edu/uniandes/csw/lostoderos/test/persistence/CalificacionPersistenceTest.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.lostoderos.test.persistence;
+
+import co.edu.uniandes.csw.lostoderos.entities.CalificacionEntity;
+import co.edu.uniandes.csw.lostoderos.entities.ContratoEntity;
+import co.edu.uniandes.csw.lostoderos.persistence.CalificacionPersistence;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+/**
+ *
+ * @author s.blancoc
+ */
+@RunWith(Arquillian.class)
+public class CalificacionPersistenceTest {
+    
+        /**
+     *
+     * @return Devuelve el jar que Arquillian va a desplegar en el Glassfish
+     * embebido. El jar contiene las clases de Calificacion, el descriptor de la
+     * base de datos y el archivo beans.xml para resolver la inyección de
+     * dependencias.
+     */
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addPackage(CalificacionEntity.class.getPackage())
+                .addPackage(CalificacionPersistence.class.getPackage())
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    /**
+     * Inyección de la dependencia a la clase CalificacionPersistence cuyos métodos
+     * se van a probar.
+     */
+    @Inject
+    private CalificacionPersistence calificacionPersistence;
+    
+
+    /**
+     * Contexto de Persistencia que se va a utilizar para acceder a la Base de
+     * datos por fuera de los métodos que se están probando.
+     */
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Variable para martcar las transacciones del em anterior cuando se
+     * crean/borran datos para las pruebas.
+     */
+    @Inject
+    UserTransaction utx;
+
+    /**
+     * Configuración inicial de la prueba.
+     *
+     *
+     */
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Limpia las tablas que están implicadas en la prueba.
+     *
+     *
+     */
+    private void clearData() {
+        em.createQuery("delete from EditorialEntity").executeUpdate();
+    }
+
+    /**
+     * lista que tiene los datos de prueba
+     */
+    private List<ContratoEntity> data = new ArrayList<ContratoEntity>();
+
+    /**
+     * Inserta los datos iniciales para el correcto funcionamiento de las
+     * pruebas.
+     *
+     *
+     */
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            
+            ContratoEntity entity = factory.manufacturePojo(ContratoEntity.class);
+
+            em.persist(entity);
+            
+            data.add(entity);
+        }
+    }
+    
+    /**
+     * Prueba para crear una calificacion
+     */
+        @Test
+    public void createCalificacionTest() {
+        PodamFactory factory = new PodamFactoryImpl();
+        CalificacionEntity newEntity = factory.manufacturePojo(CalificacionEntity.class);
+        CalificacionEntity result = calificacionPersistence.create(newEntity);
+
+        Assert.assertNotNull(result);
+
+        CalificacionEntity entity = em.find(CalificacionEntity.class, result.getId());
+
+        Assert.assertEquals(newEntity.getName(), entity.getName());
+    }
+    
+}
