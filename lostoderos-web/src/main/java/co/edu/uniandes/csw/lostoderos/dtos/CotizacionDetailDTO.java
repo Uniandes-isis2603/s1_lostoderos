d21f8295442e307752f77a0a201cf085d0e4fdcf
m.saaravia <m.saravia@uniandes.edu.co>
2018-03-05 15:20:57 -0500

    me falta el constructor que recibe el entity en esa clase y el toEntity

--- lostoderos-web/src/main/java/co/edu/uniandes/csw/lostoderos/dtos/CotizacionDetailDTO.java
+++ lostoderos-web/src/main/java/co/edu/uniandes/csw/lostoderos/dtos/CotizacionDetailDTO.java
@@ -5,6 +5,8 @@
  */
 package co.edu.uniandes.csw.lostoderos.dtos;
 
+import co.edu.uniandes.csw.lostoderos.entities.CotizacionEntity;
+
 /**
  *
  * @author m.saravia
@@ -14,6 +16,21 @@ public class CotizacionDetailDTO extends CotizacionDTO {
     public CotizacionDetailDTO() {
         
         super();
+    }
+    
+    public CotizacionDetailDTO(CotizacionEntity entity){
+        
+        super(entity);
+    }
+
+    @Override
+    public CotizacionEntity toEntity() {
+        return super.toEntity(); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
     }
     
     
