74e07923d84bac02bd402b3ac9007f02766a0e1e
Sergio Naranjo Puentes <s.naranjop1@uniandes.edu.co>
2018-04-23 12:13:31 -0500

    Creacion Servicio

--- lostoderos-web/src/main/webapp/src/modules/servicios/servicios.mod.js
+++ lostoderos-web/src/main/webapp/src/modules/servicios/servicios.mod.js
@@ -1,17 +1,17 @@
 (function (ng) {
-    
+
     var mod = ng.module("serviciosModule", ['ui.router']);
-    
+
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            
+
             var basePath = 'src/modules/servicios/';
-            
+
             $urlRouterProvider.otherwise("/serviciosList");
-            
+
             $stateProvider.state('servicios', {
                 url: '/servicios',
-                abstract:true,
-                 views: {
+                abstract: true,
+                views: {
                     'mainView': {
                         templateUrl: basePath + 'servicios.html',
                         controller: 'serviciosCtrl',
@@ -20,29 +20,38 @@
                 }
             }).state('serviciosList', {
                 url: '/list',
-                parent:'servicios',
-                 views: {
-                    'listView':{
-                        templateUrl:basePath+'servicios.list.html'
-                    }
-                }
-            }).state('servicioDetail',{
-                url: '/{servicioId:int}/detail',
-                parent:'servicios',
-                param: {servicioId: null},
-                views:{
+                parent: 'servicios',
+                views: {
                     'listView': {
                         templateUrl: basePath + 'servicios.list.html'
-                        
+                    }
+                }
+            }).state('servicioDetail', {
+                url: '/{servicioId:int}/detail',
+                parent: 'servicios',
+                param: {servicioId: null},
+                views: {
+                    'listView': {
+                        templateUrl: basePath + 'servicios.list.html'
+
                     },
-                    'detailView':{
+                    'detailView': {
                         templateUrl: basePath + 'servicios.detail.html',
                         controller: 'servicioDetailCtrl',
                         controllerAs: 'ctrl'
                     }
                 }
+            }).state('serviciosCreate', {
+                url: '/create',
+                parent: 'servicios',
+                views: {
+                    'detailView': {
+                        templateUrl: basePath + '/new/servicios.new.html',
+                        controller: 'servicioNewCtrl'
+                    }
+                }
             });
-            
+
         }
     ]);
 })(window.angular);
