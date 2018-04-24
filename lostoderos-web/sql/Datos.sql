de1fc487fb6a8ab2b2777cf87c6b9c0d5d45eb0e
m.saaravia <m.saravia@uniandes.edu.co>
2018-04-23 21:53:26 -0500

    inserciones de solicitud

--- lostoderos-web/sql/Datos.sql
+++ lostoderos-web/sql/Datos.sql
@@ -6,3 +6,10 @@ insert into ServicioEntity  (nombre, categorias, descripcion, imagen) values ('P
 insert into ServicioEntity  (nombre, categorias, descripcion, imagen) values ('Gas', 'Servicios Basicos', 'Aqui va una descripcion detallada del servicio', 'data/images/Gas.png');
 insert into ServicioEntity  (nombre, categorias, descripcion, imagen) values ('Electricidad', 'Servicios Básicos', 'Aqui va una descripcion detallada del servicio', 'data/images/Electricidad.jpg');
 insert into ServicioEntity  (nombre, categorias, descripcion, imagen) values ('Mudanza', 'Servicios Premium', 'Aqui va una descripcion detallada del servicio', 'data/images/Mudanza.png');
+
+
+insert into SolicitudEntity (id, cantidad_contratistas, cod_seguridad, descripcion, fecha_inicio, requerimientos, tipo_servicio) values (23, 2, 351, 'servicio de baño', '05/03/2018', 'plomeria', 12)
+insert into SolicitudEntity (id, cantidad_contratistas, cod_seguridad, descripcion, fecha_inicio, requerimientos, tipo_servicio) values (24, 3, 323, 'servicio de antena', '05/03/2018', 'electricidad', 11)
+insert into SolicitudEntity (id, cantidad_contratistas, cod_seguridad, descripcion, fecha_inicio, requerimientos, tipo_servicio) values (25, 1, 452, 'servicio de lavamanos', '05/03/2018', 'plomeria', 12)
+insert into SolicitudEntity (id, cantidad_contratistas, cod_seguridad, descripcion, fecha_inicio, requerimientos, tipo_servicio) values (26, 2, 362, 'arreglar conexiones de cbles', '05/03/2018', 'electricidad', 11)
+insert into SolicitudEntity (id, cantidad_contratistas, cod_seguridad, descripcion, fecha_inicio, requerimientos, tipo_servicio) values (27, 2, 145, 'servicio de baño', '05/03/2018', 'plomeria', 12)
