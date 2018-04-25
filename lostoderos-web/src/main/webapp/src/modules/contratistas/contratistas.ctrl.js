55fafd9854d0da6701e76807d2fd60fabb96cf26
SERGIO ANDRES YEPES JOVEN <sa.yepes@sis.virtual.uniandes.edu.co>
2018-04-24 23:35:32 -0500

    mejora visual de la lista de contratistas. Filtro de contratistas implementado

--- lostoderos-web/src/main/webapp/src/modules/contratistas/contratistas.ctrl.js
+++ lostoderos-web/src/main/webapp/src/modules/contratistas/contratistas.ctrl.js
@@ -1,20 +1,35 @@
-    (function (ng) {
+(function (ng) {
     var mod = ng.module("contratistasModule");
     mod.constant("contratistasContext", "api/contratistas");
-    mod.controller('contratistasCtrl', ['$scope', '$state','$http', 'contratistasContext',
-        function ($scope, $state, $http, contratistasContext) {
+    mod.controller('contratistasCtrl', ['$scope', '$state', '$http', '$filter','contratistasContext',
+        function ($scope, $state, $http, $filter, contratistasContext) {
+            
             $http.get(contratistasContext).then(function (response) {
                 $scope.contratistasRecords = response.data;
+                $scope.list=response.data;
             });
-            
-            this.createContratista = function(){
+
+            this.createContratista = function () {
                 contratista = $scope.contratista;
-                return $http.post(contratistasContext,contratista).then(function(){
+                return $http.post(contratistasContext, contratista).then(function () {
                     $state.go('contratistasList');
                 });
             };
+
+            this.filtroContratistas =function(){
+              nombre = $scope.nombre;
+              
+              $scope.contratistasRecords.forEach(function(element){
+                  if(element.nombre === nombre){
+                      $scope.list.add(element);
+                  }
+              });
+              
+            };
+
+            
         }
-        
+
     ]);
 }
 )(window.angular);
