a383b2f692deab7233cadcb6a13f81b5dfb19e59
Sergio Naranjo Puentes <s.naranjop1@uniandes.edu.co>
2018-05-03 22:03:28 -0500

    Animacion barra navegacion, mejoraiento vistas de registro y servicio

--- lostoderos-web/sql/Datos.sql
+++ lostoderos-web/sql/Datos.sql
@@ -7,7 +7,6 @@ insert into ServicioEntity  (nombre, categorias, descripcion, imagen) values ('G
 insert into ServicioEntity  (nombre, categorias, descripcion, imagen) values ('Electricidad', 'Servicios Básicos', 'Aqui va una descripcion detallada del servicio', 'data/images/Electricidad.jpg');
 insert into ServicioEntity  (nombre, categorias, descripcion, imagen) values ('Mudanza', 'Servicios Premium', 'Aqui va una descripcion detallada del servicio', 'data/images/Mudanza.png');
 
-<<<<<<< HEAD
 delete from USUARIOENTITY;
 insert into USUARIOENTITY ( dtype,nombre, correo, usuario, contrasena, reputacion) values ('ContratistaEntity' ,'Christi Lamanby', 'clamanby0@youtube.com', 'clamanby0', 'FmAhX0zkz9WQ', null);
 insert into USUARIOENTITY (dtype ,nombre, correo, usuario, contrasena, reputacion) values ( 'ContratistaEntity' ,'Gill Heisman', 'gheisman1@nymag.com', 'gheisman1', 'LOUAd68', null);
@@ -19,21 +18,6 @@ insert into USUARIOENTITY (dtype ,nombre, correo, usuario, contrasena, reputacio
 insert into USUARIOENTITY (dtype ,nombre, correo, usuario, contrasena, reputacion) values ('ContratistaEntity' , 'Redford Hugle', 'rhugle7@google.cn', 'rhugle7', 'UxYmUlEAm', null);
 insert into USUARIOENTITY (dtype ,nombre, correo, usuario, contrasena, reputacion) values ('ContratistaEntity' , 'Catrina Currie', 'ccurrie8@quantcast.com', 'ccurrie8', 'pfWjXOB', null);
 insert into USUARIOENTITY ( dtype,nombre, correo, usuario, contrasena, reputacion) values ('ContratistaEntity' , 'Karalynn Feveryear', 'kfeveryear9@fc2.com', 'kfeveryear9', 'ty4ew9k5fq', null);
-=======
-
-delete from USUARIOENTITY;
-insert into USUARIOENTITY (id, dtype,nombre, correo, usuario, contrasena, reputacion) values (1,'ContratistaEntity' ,'Christi Lamanby', 'clamanby0@youtube.com', 'clamanby0', 'FmAhX0zkz9WQ', null);
-insert into USUARIOENTITY (id,dtype ,nombre, correo, usuario, contrasena, reputacion) values (2, 'ContratistaEntity' ,'Gill Heisman', 'gheisman1@nymag.com', 'gheisman1', 'LOUAd68', null);
-insert into USUARIOENTITY (id,dtype ,nombre, correo, usuario, contrasena, reputacion) values (3,'ContratistaEntity' , 'Saundra Bonhan', 'sbonhan2@mashable.com', 'sbonhan2', 'CjeMX5YG', null);
-insert into USUARIOENTITY (id,dtype ,nombre, correo, usuario, contrasena, reputacion) values (4, 'ContratistaEntity' ,'Sybil Ivanenko', 'sivanenko3@friendfeed.com', 'sivanenko3', 'tr2esTLJ', null);
-insert into USUARIOENTITY (id,dtype ,nombre, correo, usuario, contrasena, reputacion) values (5, 'ContratistaEntity' ,'Reuben Skerman', 'rskerman4@odnoklassniki.ru', 'rskerman4', 'XS6EEX9E', null);
-insert into USUARIOENTITY (id, dtype,nombre, correo, usuario, contrasena, reputacion) values (6,'ContratistaEntity' , 'Francis Mum', 'fmum5@loc.gov', 'fmum5', 'DKKORXeqYPni', null);
-insert into USUARIOENTITY (id,dtype ,nombre, correo, usuario, contrasena, reputacion) values (7, 'ContratistaEntity' ,'Noam Leeds', 'nleeds6@free.fr', 'nleeds6', 'yWFNDCET9TAU', null);
-insert into USUARIOENTITY (id,dtype ,nombre, correo, usuario, contrasena, reputacion) values (8,'ContratistaEntity' , 'Redford Hugle', 'rhugle7@google.cn', 'rhugle7', 'UxYmUlEAm', null);
-insert into USUARIOENTITY (id,dtype ,nombre, correo, usuario, contrasena, reputacion) values (9,'ContratistaEntity' , 'Catrina Currie', 'ccurrie8@quantcast.com', 'ccurrie8', 'pfWjXOB', null);
-insert into USUARIOENTITY (id, dtype,nombre, correo, usuario, contrasena, reputacion) values (10,'ContratistaEntity' , 'Karalynn Feveryear', 'kfeveryear9@fc2.com', 'kfeveryear9', 'ty4ew9k5fq', null);
->>>>>>> origin/master
-
 
 delete from PAGOENTITY;
 insert into PAGOENTITY (ID, CODIGOTARJETA, COMPROBANTEPAGOMEDIO, COMPROBANTEPAGOTOTAL, DESCRIPCION, FECHATARJETA,NAME, NUMTARJETA) values (100, '123', 1, 1,  'pago de tarjeta con num 123',  '19/02/2018', null, '123456789123');
